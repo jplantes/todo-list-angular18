144e1f8539c8d2ec69b8fe2f4a393c349df08d71
Juan Pablo Pérez Lantes <122309224+jplantes@users.noreply.github.com>
2024-07-04 12:37:05 -0300

    se quito el Store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
